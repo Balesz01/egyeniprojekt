f7b10b32ee7fceefb3e1be650f81cf7ce478ffba
Balesz01 <144881386+Balesz01@users.noreply.github.com>
2026-05-07 17:59:28 +0200

    dobas mechanika kisebb nagyobb eldöntés és történet hozzáadás

--- dobokocka.js
+++ dobokocka.js
@@ -3,15 +3,28 @@ function randomgen() {
     return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
 }
 
-
 const container = document.getElementById('kockadiv');
+const nyertestext = document.getElementById('nyertes');
+const tortenet = document.getElementById('tortenetdiv');
 
-const Div1 = document.createElement('div');
-Div1.innerHTML("")
+document.getElementById('dobasGomb').addEventListener('click', () => {
+    container.innerHTML = '';
 
-const Div2 = document.createElement('div');
-Div2.textContent = 'Szia! Én egy új elem vagyok.';
-Div2.style.color = 'blue';
+    const img1 = document.createElement('img');
+    let dobas1 = randomgen();
+    img1.src = `img/dob${dobas1}.png`; 
 
 
-container.appendChild(Div1,Div2);
+    const img2 = document.createElement('img');
+    let dobas2 = randomgen();
+    img2.src = `img/dob${dobas2}.png`;
+
+    let nagyobb = (dobas1 > dobas2) ? "Az első játékos nyert" : (dobas1 < dobas2) ? "A második játékos nyert" : "Döntetlen"
+    nyertestext.innerHTML = nagyobb;
+
+    const tort = document.createElement('p');
+    tort.innerText = `Játékos 1 dobása: ${dobas1} és Játékos 2 dobása: ${dobas2} és a kimenetel  ${nagyobb}`;
+
+    container.append(img1, img2);
+    tortenet.append(tort);
+});
